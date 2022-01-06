5e81606fc9db53f5f1e761444935251478998b52
BilalKarakan <bilalkaraka36@gmail.com>
2022-01-07 01:14:06 +0300

    replaceChild kullanımı

---
+++ Javascript_Studies/replaceChild/dom.js
@@ -0,0 +1,7 @@
+var heading = document.createElement("h2");
+var node2 = document.createTextNode("Hello JavaScript!");
+heading.appendChild(node2);
+
+alert("Changes");
+var p1 = document.getElementById("p1");
+div1.replaceChild(heading, p1);
