846fcef262959a298903618f68e979835c01229a
BilalKarakan <bilalkaraka36@gmail.com>
2022-01-18 12:27:30 +0300

    18.01.2022

---
+++ Typescript_Studies/intro.ts
@@ -0,0 +1,18 @@
+// Javascript
+/*
+function greeter(firstName){
+    return "Hello " + firstName    
+}
+let message1 = greeter("Bilal")
+let message2 = greeter(12345)
+let message3 = greeter(["Bilal", "Elif"])
+*/
+
+// Typescript
+function greeter2(firstName:string){
+    return "Hello " + firstName
+}
+
+let message4 = greeter2("Bilal")
+//let message5 = greeter2(124)
+//let message6 = greeter2(["Bilal", "Elif"])
