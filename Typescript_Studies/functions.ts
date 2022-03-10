4838ce23e687cbf5e88aab644df5de2d41b07c44
BilalKarakan <bilalkaraka36@gmail.com>
2022-03-11 02:43:28 +0300

    11.03.2022

--- Typescript_Studies/functions.ts
+++ Typescript_Studies/functions.ts
@@ -14,3 +14,20 @@ console.log(addition(4,5))
 console.log(addition(2,"Ankara"))
 //console.log(addition2(5,"Bursa"))
 console.log(addition3(6,7))
+
+function addition4(x:number, y:number = 4):number{
+    return x+y;
+}
+
+console.log(addition4(3,8))    // x = 3 , y = 8
+console.log(addition4(3))      // x = 3 , y = 4
+
+function addition5(x:number, y?:number):number{
+    if(y){
+        return x+y; 
+    }
+    return x;
+}
+
+console.log(addition5(3))
+console.log(addition5(3,5))
